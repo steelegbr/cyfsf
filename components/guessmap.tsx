e212990c69acf96d5ce68ac6807b772710e4e287
Marc Steele <steelegbr@gmail.com>
2023-05-14 20:51:42 +0100

    We can now get a guess from the punters.

---
+++ components/guessmap.tsx
@@ -0,0 +1,43 @@
+import { useMemo, useRef, useState } from "react";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+
+interface MapProps {
+    center: Coordinate;
+    zoom: number;
+    guess: Coordinate,
+    setGuess: Function
+}
+
+const GuessMap = (props: MapProps) => {
+    const { center, zoom, guess, setGuess } = props;
+    const markerRef = useRef(null);
+
+    const eventHandlers = useMemo(
+        () => ({
+            dragend() {
+                const marker = markerRef.current as any;
+                if (marker != null) {
+                    setGuess(marker.getLatLng())
+                }
+            },
+        }),
+        [],
+    )
+
+    return (
+        <MapContainer center={center} zoom={zoom} scrollWheelZoom={true} className="map">
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker
+                eventHandlers={eventHandlers}
+                draggable={true}
+                ref={markerRef}
+                position={guess}
+            />
+        </MapContainer>
+    )
+}
+
+export default GuessMap;
--- pages/play.tsx
+++ pages/play.tsx
@@ -1,15 +1,25 @@
 import dynamic from "next/dynamic";
+import { useCallback, useState } from "react";
 
-const Map = dynamic(() => import("../components/map"), { ssr: false });
-const ukCentre = [54.003644, -2.5478587];
+const GuessMap = dynamic(() => import("../components/guessmap"), { ssr: false });
+const ukCentre: Coordinate = [54.003644, -2.5478587];
 const startZoom = 5;
 
 const Play = () => {
+    const [guess, setGuess] = useState(ukCentre);
+
+    const handleGuess = useCallback(
+        () => {
+            console.log(`Guess: ${guess}`);
+        },
+        [guess]
+    )
+
     return (
         <>
-            <Map center={ukCentre} zoom={startZoom} />
+            <GuessMap center={ukCentre} zoom={startZoom} guess={guess} setGuess={setGuess} />
             <div className="text-center">
-                <button className="guessButton">Right Around Here!</button>
+                <button className="guessButton" onClick={handleGuess}>Right Around Here!</button>
             </div>
         </>
     )
