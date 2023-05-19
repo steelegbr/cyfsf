ed4f5b47b9c3c14071a0441f8ad1ebe00dd8657a
Marc Steele <steelegbr@gmail.com>
2023-05-19 07:55:11 +0100

    Typo

--- components/layout.tsx
+++ components/layout.tsx
@@ -13,7 +13,7 @@ const Layout = (props: LayoutProps) => {
                 <title>Can You Find Sherwood Forest</title>
                 <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"
                     integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI="
-                    crossorigin="" />
+                    crossOrigin="" />
             </Head>
             <Menu />
             <div className="container mx-auto flex flex-col space-y-4">
