8f5b40d7b2ad5cdc07f00b5393277fa62fdcf781
Marc Steele <steelegbr@gmail.com>
2023-05-19 18:46:27 +0100

    Change to Vercel analytics.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,15 @@ import Layout from "@/components/layout"
 import { AppProps } from "next/app"
 import "tailwindcss/tailwind.css"
 import "../styles/global.css"
-import { GoogleAnalytics } from "nextjs-google-analytics"
+import { Analytics } from '@vercel/analytics/react';
 
 const App = (props: AppProps) => {
     const { Component, pageProps } = props;
     return (
         <Layout>
             <>
-                <GoogleAnalytics trackPageViews />
                 <Component {...pageProps} />
+                <Analytics />
             </>
         </Layout>
     )
