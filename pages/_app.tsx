390c2676884ebc752ad4aca4fa46f88b08c559d4
Marc Steele <steelegbr@gmail.com>
2023-05-19 07:54:13 +0100

    Google analytics.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,13 @@ import Layout from "@/components/layout"
 import { AppProps } from "next/app"
 import "tailwindcss/tailwind.css"
 import "../styles/global.css"
+import { GoogleAnalytics } from "nextjs-google-analytics"
 
 const App = (props: AppProps) => {
     const { Component, pageProps } = props;
     return (
         <Layout>
+            <GoogleAnalytics trackPageViews />
             <Component {...pageProps} />
         </Layout>
     )
